33a9de4e0fa0c81565734b8a2de4f3e4f41d77e8
Chinta Midhilesh Venkata Sai Reddy <chinta.midhilesh@gmail.com>
2024-04-13 17:11:41 +0530

    Fixed as an error

--- billing.php
+++ billing.php
@@ -1,57 +1,51 @@
 <?php session_start();
-if(!isset($_SESSION['id'])){
-	echo '<script>windows: location="index.php"</script>';
-
-
-	}
-
+if (!isset($_SESSION['id'])) {
+    echo '<script>window.location="index.php"</script>';
+}
 ?>
+
 <?php
-$session=$_SESSION['id'];
+$session = $_SESSION['id'];
 include 'db.php';
-$result = mysqli_query($conn,"SELECT * FROM user where id= '$session'");
-while($row = mysqli_fetch_array($result))
-  {
-  $sessionname=$row['Midhilesh Chinta'];
-
-  }
-
+$result = mysqli_query($conn, "SELECT * FROM user WHERE id = '$session'");
+while ($row = mysqli_fetch_array($result)) {
+    $sessionname = $row['name'];
+}
 ?>
+
 <?php
 include 'db.php';
-  $results = mysqli_query($conn,"SELECT * FROM user");
-  $users = mysqli_num_rows($results);
-?>
-<?php
-include 'db.php';
-  $results = mysqli_query($conn,"SELECT * FROM bill");
-  $bill = mysqli_num_rows($results);
-?>
-<?php
-include 'db.php';
-  $jibu = mysqli_query($conn,"SELECT * FROM owners");
-  $client = mysqli_num_rows($jibu);
+$results = mysqli_query($conn, "SELECT * FROM user");
+$users = mysqli_num_rows($results);
 ?>
 
 <?php
-if (isset($_POST['add']))
-	{
-	include 'db.php';
-			 		$id=$_POST['id'] ;
-					$lname= $_POST['lname'] ;
-					$fname=$_POST['fname'] ;
-					$mi=$_POST['mi'] ;
-					$address=$_POST['address'] ;
-					$contact=$_POST['contact'] ;
+include 'db.php';
+$results = mysqli_query($conn, "SELECT * FROM bill");
+$bill = mysqli_num_rows($results);
+?>
 
-		 mysqli_query($conn,"INSERT INTO  owners (id,lname,fname,mi,address,contact)
+<?php
+include 'db.php';
+$jibu = mysqli_query($conn, "SELECT * FROM owners");
+$client = mysqli_num_rows($jibu);
+?>
+
+<?php
+if (isset($_POST['add'])) {
+    include 'db.php';
+    $id = $_POST['id'];
+    $lname = $_POST['lname'];
+    $fname = $_POST['fname'];
+    $mi = $_POST['mi'];
+    $address = $_POST['address'];
+    $contact = $_POST['contact'];
+
+    mysqli_query($conn, "INSERT INTO  owners (id,lname,fname,mi,address,contact)
 		 VALUES ('$id','$lname','$fname','$mi','$address','$contact')");
 
-				echo '<script>alert("success")</script>';
-
-
-	        }
-
+    echo '<script>alert("success")</script>';
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -62,78 +56,89 @@ if (isset($_POST['add']))
 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
 <script type="text/javascript">
-function addCommas(nStr){
- nStr += '';
- var x = nStr.split('.');
- var x1 = x[0];
- var x2 = x.length > 1 ? '.' + x[1] : '';
- var rgx = /(\d+)(\d{3})/;
- while (rgx.test(x1)) {
-  x1 = x1.replace(rgx, '$1' + ',' + '$2');
- }
- return x1 + x2;
+function addCommas(nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
 }
-
 </script>
 <script src="lib/jquery.js" type="text/javascript"></script>
 <script src="src/facebox.js" type="text/javascript"></script>
 <script src="css/bootstrap/dist/js/jquery.js"></script>
 <script src="css/bootstrap/dist/js/bootstrap.min.js"></script>
-  <script type="text/javascript">
-	jQuery(document).ready(function($) {
-	  $('a[rel*=facebox]').facebox({
-		loadingImage : 'src/loading.gif',
-		closeImage   : 'src/closelabel.png'
-	  })
-	})
-  </script>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+    $('a[rel*=facebox]').facebox({
+        loadingImage: 'src/loading.gif',
+        closeImage: 'src/closelabel.png'
+    })
+})
+</script>
 <script src="js/application.js" type="text/javascript" charset="utf-8"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Billing System</title>
 <style type="text/css">
-#wrapper{
- width:100%;
- margin:0 auto;
- border:3px solid rgba(0,0,0,0);
--webkit-border-radius:5px;
--moz-border-radius:5px;
- border-radius:5px;
--webkit-box-shadow:0 0 18px rgba(0,0,0,0.4);
--moz-box-shadow:0 0 18px rgba(0,0,0,0.4);
- box-shadow:0 0 18px rgba(0,0,0,0.4);
- margin-top:2%;
- padding:10px;
- height:550px;
+#wrapper {
+    width: 100%;
+    margin: 0 auto;
+    border: 3px solid rgba(0, 0, 0, 0);
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    -webkit-box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    -moz-box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    margin-top: 2%;
+    padding: 10px;
+    height: 550px;
 }
-#header { width:900px; height:100px;}
-table th {background:#999;}
-#form {
-width:400px;
-float:left;
- border:3px solid rgba(0,0,0,0);
--webkit-border-radius:5px;
--moz-border-radius:5px;
- border-radius:5px;
--webkit-box-shadow:0 0 18px rgba(0,0,0,0.4);
--moz-box-shadow:0 0 18px rgba(0,0,0,0.4);
- box-shadow:0 0 18px rgba(0,0,0,0.4);
- margin-top:5%;
 
+#header {
+    width: 900px;
+    height: 100px;
 }
+
+table th {
+    background: #999;
+}
+
+#form {
+    width: 400px;
+    float: left;
+    border: 3px solid rgba(0, 0, 0, 0);
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    -webkit-box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    -moz-box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    margin-top: 5%;
+}
+
 #ryt {
-float:right;
- border:3px solid rgba(0,0,0,0);
--webkit-border-radius:5px;
--moz-border-radius:5px;
- border-radius:5px;
--webkit-box-shadow:0 0 18px rgba(0,0,0,0.4);
--moz-box-shadow:0 0 18px rgba(0,0,0,0.4);
- box-shadow:0 0 18px rgba(0,0,0,0.4);
- margin-top:5%;
+    float: right;
+    border: 3px solid rgba(0, 0, 0, 0);
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    -webkit-box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    -moz-box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    box-shadow: 0 0 18px rgba(0, 0, 0, 0.4);
+    margin-top: 5%;
 }
-#header ul li{
-	list-style:none;
-	float:left; margin-top:30px; margin-left:10px;}
+
+#header ul li {
+    list-style: none;
+    float: left;
+    margin-top: 30px;
+    margin-left: 10px;
+}
 </style>
 </head>
 
@@ -142,7 +147,7 @@ float:right;
 <div id="wrapper">
   <h1><center><b>Internet Billing System</b></center></h1>
   <div style="color:#F00; font-size:12px; margin-left:900px;">
-  <span>Midhilesh Chinta</span><a href="logout.php">   <span class="btn btn-danger  glyphicon glyphicon-log-out">&nbsp;Logout</span></a>
+  <span><?php echo $sessionname; ?></span><a href="logout.php">   <span class="btn btn-danger  glyphicon glyphicon-log-out">&nbsp;Logout</span></a>
   </div>
   <ul class="nav nav-pills">
     <li class="btn btn-default btn-xs"><a href="billing.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Home</a></li>
@@ -154,7 +159,7 @@ float:right;
   <div class="tab-content">
     <div id="home" class="tab-pane fade in active">
     <!-------- home panel ----------------------------->
-      <h4>Welcome Midhilesh Chinta</4>
+      <h4>Welcome <?php echo $sessionname; ?></4>
       <hr color="#000000" />
 
       <div class="col-md-4">
@@ -176,7 +181,7 @@ float:right;
                 <div class="panel-title"><h5 style="font-size:24px; font-weight:bold;">Users</h5></div>
             </div>
               <div class="panel-body">
-               <i><h1 align="center" style="font:Verdana, Geneva, sans-serif; font-weight:bolder;"><?php echo $users; ?></h1>
+               <h1 align="center" style="font:Verdana, Geneva, sans-serif; font-weight:bolder;"><?php echo $users; ?></h1>
               </div>
               <a href="user.php"><div class="panel-footer"><span class="alert-link glyphicon glyphicon-circle-arrow-right"></span>View</div></a>
          </div>
@@ -189,21 +194,16 @@ float:right;
                 <div class="panel-title"><h5 style="font-size:24px; font-weight:bold;">Bills and Income</h5></div>
             </div>
               <div class="panel-body">
-              <h1 align="center" style="font:Verdana, Geneva, sans-serif; font-weight:bolder;"><?php echo $bill; ?>
+              <h1 align="center" style="font:Verdana, Geneva, sans-serif; font-weight:bolder;"><?php echo $bill; ?></h1>
               <?php
               include "db.php";
-
 			  $add=mysqli_query($conn,"Select SUM(price) from Bill");
-              while($row1=mysqli_fetch_array($add))
-               {
-                 $total=$row1['SUM(price)'];
-                  }
-
-
-			  ?>
-              </h1>
+              while($row1=mysqli_fetch_array($add)) {
+                  $total=$row1['SUM(price)'];
+              }
+              ?>
               </div>
-             <a href="bill.php"> <div class="panel-footer"><span class="alert-link glyphicon glyphicon-circle-arrow-right"></span>View</div></a>
+             <a href="bill.php"><div class="panel-footer"><span class="alert-link glyphicon glyphicon-circle-arrow-right"></span>View</div></a>
          </div>
       </div>
     </div>
@@ -215,40 +215,36 @@ float:right;
 </body>
 
 </html>
- <script src="js/jquery.js"></script>
-  <script type="text/javascript">
+<script src="js/jquery.js"></script>
+<script type="text/javascript">
 $(function() {
+    $(".delbutton").click(function() {
+        //Save the link in a variable called element
+        var element = $(this);
 
+        //Find the id of the link that was clicked
+        var del_id = element.attr("id");
 
-$(".delbutton").click(function(){
+        //Built a url to send
+        var info = 'id=' + del_id;
+        if (confirm("Sure you want to delete this update? There is NO undo!")) {
 
-//Save the link in a variable called element
-var element = $(this);
+            $.ajax({
+                type: "GET",
+                url: "delete.php",
+                data: info,
+                success: function() {
 
-//Find the id of the link that was clicked
-var del_id = element.attr("id");
+                }
+            });
+            $(this).parents(".record").animate({ backgroundColor: "#fbc7c7" }, "fast")
+                .animate({ opacity: "hide" }, "slow");
 
-//Built a url to send
-var info = 'id=' + del_id;
- if(confirm("Sure you want to delete this update? There is NO undo!"))
-		  {
+        }
 
- $.ajax({
-   type: "GET",
-   url: "delete.php",
-   data: info,
-   success: function(){
+        return false;
 
-   }
- });
-         $(this).parents(".record").animate({ backgroundColor: "#fbc7c7" }, "fast")
-		.animate({ opacity: "hide" }, "slow");
-
- }
-
-return false;
-
-});
+    });
 
 });
 </script>
